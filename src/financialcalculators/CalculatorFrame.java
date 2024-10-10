a84a4ba54f7e4d7fd7b2739fe0d187a2acfc514e
SammyLoon <140681172+SammyLoon@users.noreply.github.com>
2024-10-10 12:07:58 -0400

    I think this works

--- src/financialcalculators/CalculatorFrame.java
+++ src/financialcalculators/CalculatorFrame.java
@@ -155,4 +155,5 @@ public class CalculatorFrame extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 
+    //Hello :)
 }
