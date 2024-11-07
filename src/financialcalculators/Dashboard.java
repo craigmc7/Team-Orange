dfc56072094e4cce7ac5c0339e46ac45fae62e19
SammyLoon <140681172+SammyLoon@users.noreply.github.com>
2024-11-07 11:44:10 -0500

    Added Mortgage Calculator swing controls to dashboard.

--- src/financialcalculators/Dashboard.java
+++ src/financialcalculators/Dashboard.java
@@ -478,6 +478,8 @@ public class Dashboard extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        double initAmount=Double.parseDouble(jTextField8.getText()); // Principal Amount
+        double interestRate=Double.parseDouble(jTextField10.getText()); //interest
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jTextField8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField8ActionPerformed
