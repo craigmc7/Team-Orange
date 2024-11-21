d9e4f449c746de513c833c5fd36fde1914d0920f
camrenblake3 <camren.blake@my.maryvillecollege.edu>
2024-11-21 12:23:08 -0500

    Current SCraper

---
+++ financialcalculators/src/main/java/financialcalculators/CurrencyConverter.java
@@ -0,0 +1,28 @@
+package financialcalculators;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+import financialcalculators.CCFrame;
+
+
+
+/**
+ *
+ * @author adari
+ */
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new CCFrame().setVisible(true);
+            
+        });
+    }
+}
+
